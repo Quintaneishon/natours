4901ce5c6fdafea25c53aa4e8e940dac6dc4ca4a
Quintaneishon <ajitzi.quintana@gmail.com>
2020-05-15 23:38:04 -0500

    Updates

---
+++ precache-manifest.d001d69681f3d08bb9aea4b669b0c721.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3cfdfb23b40bb5eb6daa717313de4c2",
+    "url": "/natours/index.html"
+  },
+  {
+    "revision": "9329e48f940b051bb2d1",
+    "url": "/natours/static/css/main.28d4ce83.chunk.css"
+  },
+  {
+    "revision": "17b03ba1c8f1eb2a0686",
+    "url": "/natours/static/js/2.0eedbe52.chunk.js"
+  },
+  {
+    "revision": "81c1ab2ba64ac9f85d481dd74a981dc6",
+    "url": "/natours/static/js/2.0eedbe52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9329e48f940b051bb2d1",
+    "url": "/natours/static/js/main.fe249e68.chunk.js"
+  },
+  {
+    "revision": "edd976275937a65869eb",
+    "url": "/natours/static/js/runtime-main.6852b1cb.js"
+  },
+  {
+    "revision": "177764eadf43efc393ebb98329a774b2",
+    "url": "/natours/static/media/linea-basic-10.177764ea.svg"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/natours/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/natours/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/natours/static/media/linea-basic-10.e6763f00.ttf"
+  }
+]);
